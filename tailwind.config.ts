641053b1e08a8ba1ee4824e551b467451189e837
Guilherme <65544746+Guilherme-ags@users.noreply.github.com>
2025-01-08 10:42:10 -0300

    - Configuração Inicial do Projeto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,18 @@
 import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+const config: Config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    safelist: [
+        { pattern: /^gap-/ },
+        { pattern: /^w-/ },
+        { pattern: /^h-/ },
+        { pattern: /^text-/ },
+        { pattern: /^bg-/ },
+    ],
+    plugins: [require("tailwind-scrollbar")],
+};
+module.exports = config;
